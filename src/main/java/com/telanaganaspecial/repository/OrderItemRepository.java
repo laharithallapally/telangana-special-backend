e9eaa4433557747cb81a33bcd004b5b63136ec09
laharithallapally <laharithallapally27@gmail.com>
2026-06-24 17:15:49 +0530

    Use environment variables for datasource and secrets

--- src/main/java/com/telanaganaspecial/repository/OrderItemRepository.java
+++ src/main/java/com/telanaganaspecial/repository/OrderItemRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
     void deleteByProduct(Product product);
 
-    
+
 }
